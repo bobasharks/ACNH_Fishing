c517920a0f0725ce93fc6b6ab1e6f8a466a46538
bobasharks <130084813+bobasharks@users.noreply.github.com>
2023-11-14 17:23:58 -0800

    created terrain + textured

--- acnh fishing exploration/Assets/Scripts/PlayerController.cs
+++ acnh fishing exploration/Assets/Scripts/PlayerController.cs
@@ -11,27 +11,40 @@ public class PlayerController : MonoBehaviour
     PlayerMovement playerMovement;
 
     private float forward;
+    private float speed = 4f;
+    private bool moving = false;
+    private Vector2 left, right, up, down;
 
     private void Awake()
     {
         playerMovement = new PlayerMovement();
 
         playerMovement.Enable();
+
+        /*
+        up = new Vector2(0, 1);
+        down = new Vector2(0, -1);
+        left = new Vector2(-1, 0);
+        right = new Vector2(1, 0);
+        */
     }
 
-    private void FixedUpdate()
+    private void Update()
     {
         //gets the vector2 data from the move action composite
         Vector2 moveVec = playerMovement.Controls.Walking.ReadValue<Vector2>();
+        transform.Translate(new Vector3(moveVec.x, 0, moveVec.y) * speed * Time.deltaTime);
 
-        //apply the move vector to the player
-        GetComponent<Rigidbody>().AddForce(new Vector3(moveVec.x, 0, moveVec.y) * 5f, ForceMode.Force);
+        //sets the forward variable that controls the animation states
         forward = moveVec.y;
         anim.SetFloat("Forward", forward);
     }
     public void OnMove(InputAction.CallbackContext context)
     {
+        
         Vector2 moveVec = context.ReadValue<Vector2>();
-        //GetComponent<Rigidbody>().AddForce(new Vector3(moveVec.x, 0, forward) * 5f, ForceMode.Force);
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        
+        
     }
 }
