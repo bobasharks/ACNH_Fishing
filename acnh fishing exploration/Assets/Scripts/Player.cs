d6c913f6b66197132364d1189694975500dee0e6
bobasharks <130084813+bobasharks@users.noreply.github.com>
2023-11-16 15:51:41 -0800

    made scripts, fixed walking a bit

---
+++ acnh fishing exploration/Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private bool canCast;
+
+    public void Cast()
+    {
+        //if bobber lands in water 
+        //canCast = false
+
+        //if bobber lands on the ground
+        //reverse cast animation and remove bobber
+        //cancast = true
+    }
+
+    public void ReelIn()
+    {
+
+    }
+}
--- acnh fishing exploration/Assets/Scripts/PlayerController.cs
+++ acnh fishing exploration/Assets/Scripts/PlayerController.cs
@@ -11,16 +11,18 @@ public class PlayerController : MonoBehaviour
     PlayerMovement playerMovement;
 
     private float forward;
-    private float speed = 4f;
-    private bool moving = false;
-    private Vector2 left, right, up, down;
+    private float speed = 6f;
+    public bool moving;
+    private GameObject playerModel;
+    //private Vector2 left, right, up, down;
 
     private void Awake()
     {
+        //moving = false;
         playerMovement = new PlayerMovement();
 
         playerMovement.Enable();
-
+        playerModel = transform.GetChild(0).gameObject;
         /*
         up = new Vector2(0, 1);
         down = new Vector2(0, -1);
@@ -31,13 +33,47 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
+        
         //gets the vector2 data from the move action composite
         Vector2 moveVec = playerMovement.Controls.Walking.ReadValue<Vector2>();
         transform.Translate(new Vector3(moveVec.x, 0, moveVec.y) * speed * Time.deltaTime);
 
+        if (moveVec.x <= -1)
+        {
+            forward = moveVec.x *= -1;
+            print("going left");
+            playerModel.transform.rotation = Quaternion.Euler(0, 270, 0);
+            
+        }
+        if (moveVec.x >= 1)
+        {
+            forward = moveVec.x;
+            print("going right");
+            playerModel.transform.rotation = Quaternion.Euler(0, 90, 0);
+            //moving = true;
+        }
+        if (moveVec.y >= 1)
+        {
+            forward = moveVec.y;
+            //moving = true;
+            print("going up");
+            playerModel.transform.rotation = Quaternion.Euler(0, 0, 0);
+            //moving = true;
+        }
+        if (moveVec.y <= -1)
+        {
+            forward = moveVec.y *= -1;
+            print("going down");
+            playerModel.transform.rotation = Quaternion.Euler(0, 180, 0);
+            //moving = true;
+        }
+        
+
         //sets the forward variable that controls the animation states
-        forward = moveVec.y;
+        //forward = moveVec.y;
         anim.SetFloat("Forward", forward);
+        //anim.SetBool("Walking", moving);
+
     }
     public void OnMove(InputAction.CallbackContext context)
     {
